89a298dfba906dcfdbbacc66487afd852e126add
Abu Siam <siamcse01@gmail.com>
2022-11-24 19:32:53 +0600

    get categories add

--- index.js
+++ index.js
@@ -14,16 +14,39 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.leesidy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
 
+        const categoriesCollection = client.db('mobileMart').collection('categories');
         const usersCollection = client.db('mobileMart').collection('users');
 
-        // app.get('/jwt', async (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '24h' });
-        //     res.send({ token });
-        // });
+        app.get('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '24h' });
+            res.send({ token });
+        });
+
+        //categories
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
 
         //save User
         app.post('/users', async (req, res) => {
