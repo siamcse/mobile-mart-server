15f660dbab16293fffcdf0017029d434fb2865ad
Abu Siam <siamcse01@gmail.com>
2022-11-24 23:29:35 +0600

    user role get

--- index.js
+++ index.js
@@ -64,7 +64,13 @@ async function run() {
             const user = await usersCollection.findOne(query);
             res.send({ isAdmin: user?.role === 'admin' });
         });
-        
+        //get seller
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        });
 
         //save User
         app.post('/users', async (req, res) => {
